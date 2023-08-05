4fc2b37b8463552933c899f59f487fea03ba9608
Ersin <117252660+ErsinAgirman@users.noreply.github.com>
2023-08-05 20:38:33 +0300

    Add files via upload

---
+++ Fly.cs
@@ -0,0 +1,69 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Fly : MonoBehaviour
+{
+    [SerializeField] private float velocity,rotationSpeed;
+    [SerializeField] private Rigidbody2D rb;
+    public GameManager gameManager;
+    private bool isStarted= false;
+    public static int score = 0;
+    private Score scoreScript;
+    
+
+    
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        rb.GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0; // Başlangıçta yerçekimini devre dışı bırak
+        scoreScript = FindObjectOfType<Score>();
+        score = 1;
+        gameManager = FindObjectOfType<GameManager>(); // GameManager nesnesine erişimi burada yapın
+    }
+
+    void Update()
+    {
+         if (!isStarted && Input.GetMouseButtonDown(0))
+        {
+            isStarted = true;
+            rb.gravityScale = 1; // Tıklandığında yerçekimini etkinleştir
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            //Jump
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+
+    private void FixedUpdate() 
+    {
+        transform.rotation = Quaternion.Euler(0, 0, rb.velocity.y * rotationSpeed);
+    }
+
+    
+     private void OnCollisionEnter2D(Collision2D other) 
+    {   if (other.gameObject.CompareTag ("DeathArea"))
+        {
+            gameManager.GameOver();
+        }
+    }
+    
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("ScoreArea"))
+        {   
+            scoreScript.ScoreUp();
+            score++;
+            Debug.Log(score);
+        }
+    }
+
+
+}
